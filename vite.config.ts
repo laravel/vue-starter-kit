254678c860f28084d99d8c43da373ffa350be0c1
Vytautas Smilingis <vytautas@whatagraph.com>
2025-06-23 21:42:38 +0200

    Added vue dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 import { transformer } from './resources/js/lib/vite-plugin-transformer';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
 	plugins: [
+		vueDevTools({
+			appendTo: 'resources/js/app.ts',
+			launchEditor: 'phpstorm',
+		}),
 		laravel({
 			input: ['resources/js/app.ts'],
 			ssr: 'resources/js/ssr.ts',
