70ed801ab3e5a5c105aeb9628254fab27169fd15
Robert Boes <2871897+RobertBoes@users.noreply.github.com>
2025-12-08 11:10:26 +0100

    Showcase issue with disabling features

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
+import fs from 'fs';
+
+const stubPath = path.resolve(__dirname, 'resources/js/lib/_wayfinder-stub.ts');
 
 export default defineConfig({
     plugins: [
@@ -24,4 +28,24 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: [
+            {
+                find: /^@\/routes\/(.+)$/,
+                replacement: (match, p1) => {
+                    const target = path.resolve(__dirname, `resources/js/routes/${p1}/index.ts`);
+
+                    const mockPath = path.resolve(__dirname, `resources/js/mocks/routes/${p1}.ts`);
+
+                    const exists = fs.existsSync(target);
+
+                    if (! exists) {
+                        console.debug(`Resolving route module: ${p1} (${target}) -> ${exists ? 'actual file' : 'stub'}`);
+                    }
+
+                    return fs.existsSync(target) ? target : mockPath;
+                },
+            },
+        ],
+    },
 });
